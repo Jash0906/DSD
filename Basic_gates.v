261a049e0506227ab1188e722cd5f49b8fbb6172
Jashwanth <jashwanthbala09@ce.du.ac.in>
2025-11-06 21:38:13 +0530

    Update Basic_gates.v

--- Basic_gates.v
+++ Basic_gates.v
@@ -7,26 +7,6 @@ module basic_gates(input a, b, output and_out, or_out, not_a, nand_out, nor_out,
   assign xor_out  = a ^ b;
   assign xnor_out = ~(a ^ b);
 endmodule
-//////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06.11.2025 17:50:28
-// Design Name: 
-// Module Name: Basic_gates
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Basic_gates(
 
